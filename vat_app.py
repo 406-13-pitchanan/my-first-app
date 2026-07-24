c3491cad3e06ac5137f7e74d951e8b559813ca39
406-13-pitchanan <54358@yupparaj.ac.th>
2026-07-24 10:48:56 +0700

    vat_app.py

--- vat_app.py
+++ vat_app.py
@@ -6,4 +6,4 @@ net_price = price + vat
 st.divider()
 st.write (" นางสาวพิชชานันทน์ วรรณก้อน เลขที่ 13 ม.4/6")
 st.header ( f"๓าษีมูลค่าเพิ่ม ( VAT 7% ): {vat:2f} บาท")
-st. header (f:ราคาสุทธิ:{net_price:.2f} บาท"}
+st.header(f"• ราคาสุทธิ: {net_price:.2f} บาท")
